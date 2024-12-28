3f8bb74e9bb6faa68b7db67f0dadacb309a0036b
nomadka <noushiq.k.a@gmail.com>
2024-12-28 22:36:36 +0100

    2nd commit

--- 00.PyTorch Fundamentals.py
+++ 00.PyTorch Fundamentals.py
@@ -1,2 +1,18 @@
+## 00. PyTorch Fundamentals
+# Importing pyTorch Library
 import torch
 torch.__version__
+
+# Scalars
+scalar = torch.tensor(7)
+scalar
+#Checking the dimension of scalar
+scalar.ndim ##Scalar doesnt have any dimension, so the output will be 0
+
+# Vector
+
+vector = torch.tensor([7, 7])
+vector
+#Checking the dimension of vector
+vector.ndim ## Vector has 1 dimension
+vector.shape ## checking the shape of vector 

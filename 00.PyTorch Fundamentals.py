905bbab697ab030817eec6ae97b948e4cecdbef4
nomadka <noushiq.k.a@gmail.com>
2025-01-08 15:30:23 +0100

    tensor data type

--- 00.PyTorch Fundamentals.py
+++ 00.PyTorch Fundamentals.py
@@ -34,8 +34,35 @@ TENSOR.ndim ## Tensor has 3 dimension
 TENSOR.shape ## checking the shape of Tensor
 TENSOR[0,1]
 
-#Random Tensors
+#Random Tensors -> Why = Start with random numbers -> Look at data -> Update random number -> look at data 
 random_tensor = torch.rand(2,3,2)
 random_tensor
+# Random tensor with similar image shape
 random_image_size_tensor = torch.rand(size=(224,224,3)) # height, width, color channel
 random_image_size_tensor.shape, random_image_size_tensor.ndim
+
+# Tensors with all Zeros
+zeros = torch.zeros(size=(3,5))
+zeros
+# Create a tensor of all ones
+ones = torch.ones(size=(3,4))
+ones
+# Operations in tensor
+ones_plus_one = ones + 1
+ones_plus_one
+ones_mul_two = ones * 2
+ones_mul_two
+
+# Creating range of tensors
+one_to_ten = torch.arange(start=1,end=20,step=2)
+one_to_ten
+
+# Creating tensors like
+ten_zeros = torch.zeros_like(input = one_to_ten)
+ten_zeros
+
+# Tensor Data Type
+# Float 32 Tensor
+float_32_tensor = torch.tensor([3.0, 6.0, 9.0], dtype=None, device=None, requires_grad=False)
+float_32_tensor
+float_32_tensor.dtype
